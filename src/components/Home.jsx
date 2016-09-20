294bd2520e8cc64f60d4b92bcecfe27deaed43e8
Alex Slobodyansky <a123@123s-MacBook-Pro.local>
2016-09-20 22:24:09 +0300

    add links to social profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,11 +15,11 @@ const Home = React.createClass({
       		<h2>Фронтэнд разработчик</h2>
       		<h3>Создание сайтов \ расширение функционала \ внесение исправлений  </h3>
       		<ul className="social">
-      			<li><a href="#"><FontAwesome className="fa fa-vk" name="vkontakte" /></a></li>
-      			<li><a href="#"><FontAwesome className="fa fa-facebook" name="facebook" /></a></li>
-      			<li><a href="#"><FontAwesome className="fa fa-linkedin" name="linkedin" /></a></li>
-      			<li><a href="#"><FontAwesome className="fa fa-github" name="github" /></a></li>
-      			<li><a href="#"><FontAwesome className="fa fa-twitter" name="twitter" /></a></li>
+      			<li><a href="https://vk.com/slobodianskii" target="_blank"><FontAwesome className="fa fa-vk" name="vkontakte" /></a></li>
+      			<li><a href="https://www.facebook.com/aslobodyansky" target="_blank"><FontAwesome className="fa fa-facebook" name="facebook" /></a></li>
+      			<li><a href="https://ru.linkedin.com/in/web2ls" target="_blank"><FontAwesome className="fa fa-linkedin" name="linkedin" /></a></li>
+      			<li><a href="https://github.com/web2ls" target="_blank"><FontAwesome className="fa fa-github" name="github" /></a></li>
+      			<li><a href="https://twitter.com/quantumofweb" target="_blank"><FontAwesome className="fa fa-twitter" name="twitter" /></a></li>
       		</ul>
       	</div>
       </div>
