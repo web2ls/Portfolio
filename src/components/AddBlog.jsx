a506c1b02db4c60530a68ad3de58bb25d491dff8
web2ls <skurel@yandex.ru>
2016-09-23 17:53:32 +0300

    Add some admin pages

---
+++ src/components/AddBlog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddBlog = React.createClass({
+	render: function() {
+		return(
+			<div>
+				Add new blog here!
+			</div>
+		)
+	}
+});
+
+export default AddBlog;
---
+++ src/components/AddWork.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import database from '../core.js';
+
+const AddWork = React.createClass({
+	handleAddNewWork: function() {
+		let newWork = {
+			workName: this.refs.workName.value,
+			workDescription: this.refs.workDescription.value,
+			workLink: this.refs.workLink.value
+		};
+		database.ref('works').push(newWork);
+	},
+
+	render: function() {
+		return(
+			<div>
+				<form>
+					<fieldset>
+						<legend>Добавить новый проект: </legend>
+						<label>
+							Наименование проекта: <input ref='workName' type='text' />
+						</label><br />
+						<label>
+							Описание проекта: <input ref='workDescription' type='text' />
+						</label><br />
+						<label>
+							Ссылка на проект: <input ref='workLink' type='text' />
+						</label>
+						<br />
+						<button onClick={this.handleAddNewWork} type='button'>Добавить проект</button>
+					</fieldset>
+				</form>
+			</div>
+		)
+	}
+});
+
+export default AddWork;
